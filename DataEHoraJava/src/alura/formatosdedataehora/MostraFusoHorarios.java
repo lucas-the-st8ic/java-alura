3ffe16c36966e1a401c97ae02b8483a684d4c55d
Lucas Silva <lucas.silva.s1998@gmail.com>
2025-12-20 16:15:42 -0300

    Create MostraFusoHorarios.java

---
+++ DataEHoraJava/src/alura/formatosdedataehora/MostraFusoHorarios.java
@@ -0,0 +1,25 @@
+package alura.formatosdedataehora;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class MostraFusoHorarios {
+    static void main(String[] args) {
+
+
+        ZonedDateTime horaAtual = ZonedDateTime.now();
+
+        ZonedDateTime horarioToquio = horaAtual.withZoneSameInstant(ZoneId.of("Asia/Tokyo"));
+
+        ZonedDateTime horarioNovaYork = horaAtual.withZoneSameInstant(ZoneId.of("America/New_York"));
+
+
+        DateTimeFormatter formatacaoHora = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+
+
+        System.out.println("Horário Atual em Tóquio: " +horarioToquio.format(formatacaoHora));
+        System.out.println("Horário atual em Nova York: " +horarioNovaYork.format(formatacaoHora));
+    }
+}
