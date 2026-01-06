27011e22ab7c708bc1c98dd9429d4a048ad635b6
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-01-06 17:13:54 -0300

    Update Main.java

--- PraticandoEncapsulamento/src/Main.java
+++ PraticandoEncapsulamento/src/Main.java
@@ -60,6 +60,8 @@ public class Main {
         matrix.adicionarAvaliacao(5);
         matrix.adicionarAvaliacao(3);
         matrix.adicionarAvaliacao(4);
+        matrix.adicionarAvaliacao(7);
+        matrix.adicionarAvaliacao(2);
 
         System.out.printf("Média de avaliações para %s: %.2f\n", matrix.getTitulo(), matrix.calcularMedia());
 
