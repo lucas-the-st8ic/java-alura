74c0d3e282fad89cacd06324e97babf75b3559e1
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-01-13 15:09:38 -0300

    Create Aluno.java

---
+++ PraticandoHerancaInterfaceEPolimorfismo/src/br/com/Alura/Aluno.java
@@ -0,0 +1,11 @@
+package br.com.Alura;
+
+public class Aluno {
+    private String nomeDoAluno;
+    private String tipoMatricula;
+
+    public Aluno(String nomeDoAluno, String tipoMatricula) {
+        this.nomeDoAluno = nomeDoAluno;
+        this.tipoMatricula = tipoMatricula;
+    }
+}
