5143b6899ff3c2790a030efbb23f801f8b45b04a
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-01-20 12:08:03 -0300

    Update Main.java

--- ExemploStringsERegex/src/Main.java
+++ ExemploStringsERegex/src/Main.java
@@ -1,3 +1,6 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     static void main(String[] args) {
         String aluno = "Lucas Silva";
@@ -9,13 +12,36 @@ public class Main {
                     na anhembi morumbi
                     """;
 
-        String texto = String.format("Curso: %s %nAluno: %s ", curso, aluno);
+        String txt= String.format("Curso: %s %nAluno: %s ", curso, aluno);
 
-        System.out.println(texto);
+        System.out.println(txt);
         System.out.println("=================");
         System.out.printf("Nome: %s %nCurso: %s \n", aluno, curso);
 
         System.out.println(curriculo);
 
+        String texto = "Meu email é lucas@gmail.com";
+        Pattern pattern = Pattern.compile("\\w+@\\w+.\\w+");
+        Matcher matcher = pattern.matcher(texto);
+
+        if(matcher.find()){
+            System.out.println(matcher.group());
+        }
+
+
+        System.out.println(formatartelefone("11957387490"));
+    }
+
+
+    public static String formatartelefone(String telefone) {
+        String regex = "(\\d{2})(\\d{4,5})(\\d{4})";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(telefone);
+
+        if(matcher.matches()){
+            return String.format("Telefone: (%s) %s-%s", matcher.group(1), matcher.group(2), matcher.group(3));
+        }
+
+        return "Número de telefone inválido!!";
     }
 }
