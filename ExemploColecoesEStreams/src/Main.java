ba053f60315195daab087209b8f6aa8283a9cfea
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-01-22 10:19:25 -0300

    updates

---
+++ ExemploColecoesEStreams/src/Main.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class Main {
+    static void main(String[] args) {
+        //          -List, Set, Map-
+        /*  List - ordenação, permite duplicidade
+        *   Implementações - ArrayList e LinkedList
+        *   Operações comuns - add, remove, contains, get, size
+        * ===================================================
+        *   Set - Não se preocupa com ordenação, não permite duplicidade
+        *   Implementações: HashSet(não se preocupa com ordenação nem duplicidade)
+        *   LinkedHashSet(mantem ordenação de inserção), TreeSet(permite ordenação)
+        * ===================================================
+        *   Map - chave-valor, sem valores duplicados
+        *   Implementações: HashMap, LinkedHashMap, TreeMap
+        *   Operações comuns - put, get, remove, containsKey, keySet
+        *   sobrescreve valores com chaves iguais.
+        *   Exemplo: 1, Maria- 2, João - 1, Carlos (Carlos Sobrescreve Maria)
+        * */
+        List<String> listaDeFuncionarios = new ArrayList<>();
+
+        listaDeFuncionarios.add("Lucas");
+        listaDeFuncionarios.add("Jacqueline");
+        listaDeFuncionarios.add("Paulo");
+        listaDeFuncionarios.add("Paulo");
+
+        System.out.println(listaDeFuncionarios);
+
+
+        Set<String> produtos = new HashSet<>();
+
+        produtos.add("Fone Bluetooth");
+        produtos.add("Mouse Gamer");
+        produtos.add("Five Gears");
+        produtos.add("Five Gears");
+
+        System.out.println(produtos);
+
+
+        Map<Integer, String> listaDeClientes = new HashMap<>();
+        listaDeClientes.put(1, "Lucas");
+        listaDeClientes.put(2, "Jacqueline");
+        listaDeClientes.put(3, "Paulo");
+        listaDeClientes.put(4, "Marcos");
+
+        System.out.println(listaDeClientes.get(4));
+
+    }
+
+}
