e53da55278fc6b56e79efcfffa12c59bab931918
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-03-11 18:24:40 -0300

    updates

---
+++ DesafioTabelaFipe/TabelaFipe/src/main/java/br/com/Alura/TabelaFipe/service/ConverteDados.java
@@ -0,0 +1,11 @@
+package br.com.Alura.TabelaFipe.service;
+
+public class ConverteDados implements IConverteDados {
+
+   private ObjectMapper mapper = new ObjectMapper()
+
+    @Override
+    public <T> T obterDados(String json, Class<T> classe) {
+        return null;
+    }
+}
