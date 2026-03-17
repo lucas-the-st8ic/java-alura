b68e3c10ef5643bbe6ec22974806246fad646654
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-03-17 11:03:42 -0300

    Create Exercicio_5.java

---
+++ ExerciciosStreamELambda/src/br/com/exerciciosstreamelambda/Exercicio_5.java
@@ -0,0 +1,20 @@
+package br.com.exerciciosstreamelambda;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Exercicio_5 {
+    public static void main(String[] args) {
+
+        List<List<Integer>> listaDeNumeros = Arrays.asList(
+                Arrays.asList(1, 2, 3, 4),
+                Arrays.asList(5, 6, 7, 8),
+                Arrays.asList(9, 10, 11, 12)
+        );
+
+        //código para filtrar e ordenar números primos
+
+
+    }
+}
+
