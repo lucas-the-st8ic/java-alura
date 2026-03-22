a86e4e441a754752ec3eb1b5aa8d24f95656eea2
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-03-22 15:19:09 -0300

    updates

--- DesafioTabelaFipe/TabelaFipe/src/main/java/br/com/Alura/TabelaFipe/TabelaFipeApplication.java
+++ DesafioTabelaFipe/TabelaFipe/src/main/java/br/com/Alura/TabelaFipe/TabelaFipeApplication.java
@@ -1,13 +1,20 @@
 package br.com.Alura.TabelaFipe;
 
+import br.com.Alura.TabelaFipe.main.Main;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TabelaFipeApplication {
+public class TabelaFipeApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TabelaFipeApplication.class, args);
 	}
 
+    @Override
+    public void run(String... args) throws Exception {
+        Main main = new Main();
+        main.exibeMenu();
+    }
 }
--- DesafioTabelaFipe/TabelaFipe/src/main/java/br/com/Alura/TabelaFipe/main/Main.java
+++ DesafioTabelaFipe/TabelaFipe/src/main/java/br/com/Alura/TabelaFipe/main/Main.java
@@ -1,12 +1,16 @@
 package br.com.Alura.TabelaFipe.main;
 
+import br.com.Alura.TabelaFipe.service.ConsumoApi;
+
 import java.util.Scanner;
 
 public class Main {
 
     private Scanner input = new Scanner(System.in);
 
-    private final String URL_BASE = "https://parallelum.com.br/api/v2";
+    private ConsumoApi consumoApi = new ConsumoApi();
+
+    private final String URL_BASE = "https://parallelum.com.br/fipe/api/v1/";
 
     public void exibeMenu () {
         var menu = """
@@ -16,21 +20,24 @@ public class Main {
                 -Caminhão
                 
                 
-                Digite uma das opções para consultar: 
+                Digite uma das opções para consultar:
                 """;
         System.out.println(menu);
         var opcao = input.nextLine();
         String endereco;
 
 
-        if (opcao.toUpperCase().contains("Carr")) {
+        if (opcao.toLowerCase().contains("carr")) {
             endereco = URL_BASE + "carros/marcas";
-        } else if(opcao.toUpperCase().contains("Mot")) {
+        } else if(opcao.toLowerCase().contains("mot")) {
             endereco = URL_BASE + "motos/marcas";
         } else {
             endereco = URL_BASE + "caminhoes/marcas";
         }
 
-        
+        var json = consumoApi.obterdados(endereco);
+        System.out.println(json);
     }
+
+
 }
